5cc3791c5f6436d4413d85a276652be719c9edfa
David Kohav <david@gizra.com>
2016-01-06 17:06:47 +0200

    Added more delay

--- visual-monitor/test/tests.js
+++ visual-monitor/test/tests.js
@@ -65,7 +65,7 @@ describe('Visual monitor testing', function() {
   it('should show the home page',function(done) {
     client
       .url('http://gizra:123@dev-huji-reg.pantheon.io')
-      .pause(10000)
+      .pause(12000)
       .webdrivercss(testName + '.homepage', {
         name: '1',
         exclude: [],
